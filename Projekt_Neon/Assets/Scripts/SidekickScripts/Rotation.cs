7ce528e15d5cbd9be2452737de9d2aaf9c4297c4
unknown <Miles@nb0655.one.materna.com>
2019-03-16 17:48:38 +0100

    test3

---
+++ Projekt_Neon/Assets/Scripts/SidekickScripts/Rotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    private GameObject _player;
+    private Player GetPlayer;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+         _player = GameObject.Find("Player");
+        GetPlayer = _player.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(GetPlayer.isMoving)
+        //{
+        //    Debug.Log("isMoving");
+
+        //}
+        //else
+        //{
+        //    Debug.Log("is not");
+        //}
+
+    }
+}
---
+++ Projekt_Neon/Assets/Scripts/SidekickScripts/SideKickFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SideKickFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform PlayerPosition;
+    private Rigidbody2D SideKickPosition;
+    private GameObject Player;
+    public float speed = 2;
+    public float distanceToPlayer;
+ 
+
+
+
+    void Start()
+    {
+    
+        Player = GameObject.Find("Player");
+ 
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+
+
+        if (Vector2.Distance(transform.position, Player.transform.position)> distanceToPlayer)
+        {
+            //anim.enabled = !anim.enabled;
+            transform.position = Vector2.MoveTowards(transform.position, Player.transform.position+ new Vector3(0,3, 0), speed * Time.deltaTime);
+           // Debug.Log(Player.transform.position);
+        }
+
+
+
+    }
+
+
+}
