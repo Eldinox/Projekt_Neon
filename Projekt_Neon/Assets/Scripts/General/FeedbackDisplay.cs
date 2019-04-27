5e83a5a2fabba4ed1acdacc0af306b8da43c71ff
Eldinox <waldemar.g94@web.de>
2019-04-27 16:27:11 +0200

    GameManager Feedback

---
+++ Projekt_Neon/Assets/Scripts/General/FeedbackDisplay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackDisplay : MonoBehaviour
+{
+    public bool getHitAnimation;
+    public bool getHitColoring;
+    public bool healthBars;
+    public bool damageNumberDisplay;
+    public bool hitSparks;
+}
