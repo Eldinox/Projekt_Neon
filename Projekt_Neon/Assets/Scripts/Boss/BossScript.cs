480cf0f674f7aa6d6ab0d625d61f29eaafdc9c51
Eldinox <waldemar.g94@web.de>
2019-04-27 18:49:02 +0200

    Boss Fight

---
+++ Projekt_Neon/Assets/Scripts/Boss/BossScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+    public GameObject[] pillars;
+    private Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Invoke("Attack1", 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Attack1()
+    {
+        anim.SetTrigger("PillarAttack1");
+    }
+}
