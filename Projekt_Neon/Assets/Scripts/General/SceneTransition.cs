4a3716ee7a68f816b2f4d44ef36857c14dc68019
BlackWolf63 <max_hammer@t-online.de>
2019-04-30 17:30:13 +0200

    Scene Transitions

--- Projekt_Neon/Assets/Scripts/General/SceneTransition.cs
+++ Projekt_Neon/Assets/Scripts/General/SceneTransition.cs
@@ -19,15 +19,32 @@ public class SceneTransition : MonoBehaviour
     		string sceneName = "";
             if(this.gameObject.name.Contains("Next"))
             {
-                if(SceneManager.GetActiveScene().name == "Level_1")sceneName = "Level_2";
-                //else if(SceneManager.GetActiveScene().name == "Level_2")sceneName = "Level_3";
-                //else if(SceneManager.GetActiveScene().name == "Level_3")sceneName = "Level_4";
+                //if(SceneManager.GetActiveScene().name == "Level_1")sceneName = "Level_2";
+                if (SceneManager.GetActiveScene().name == "Level_8 (Intro)") sceneName = "Level_6 (Nr 3)";
+                else if (SceneManager.GetActiveScene().name == "Level_6 (Nr 3)") sceneName = "Level_5 (Nr2)";
+                else if (SceneManager.GetActiveScene().name == "Level_5 (Nr2)") sceneName = "Level_4 (Nr1)";
+                else if (SceneManager.GetActiveScene().name == "Level_4 (Nr1)")sceneName = "Level_Hub1";
+                else if (SceneManager.GetActiveScene().name == "Level_Hub1")sceneName = "Level_1_22";
+                else if (SceneManager.GetActiveScene().name == "Level_1_22") sceneName = "Level_1_23";
+                else if (SceneManager.GetActiveScene().name == "Level_1_23") sceneName = "Level_1_24";
+                else if (SceneManager.GetActiveScene().name == "Level_1_24") sceneName = "Level_1_25";
+                else if (SceneManager.GetActiveScene().name == "Level_1_25") sceneName = "Level_1_21";
+                else if (SceneManager.GetActiveScene().name == "Level_1_21") sceneName = "Level_7 (Boss)";
             }
             else if(this.gameObject.name.Contains("Prev"))
             {
-                if(SceneManager.GetActiveScene().name == "Level_2")sceneName = "Level_1";
-                //else if(SceneManager.GetActiveScene().name == "Level_3")sceneName = "Level_2";
-                //else if(SceneManager.GetActiveScene().name == "Level_4")sceneName = "Level_3";
+                //if(SceneManager.GetActiveScene().name == "Level_2")sceneName = "Level_1";
+                if(SceneManager.GetActiveScene().name == "Level_6 (Nr 3)") sceneName = "Level_8 (Intro)";
+                else if (SceneManager.GetActiveScene().name == "Level_5 (Nr2)") sceneName = "Level_6 (Nr 3)";
+                else if (SceneManager.GetActiveScene().name == "Level_4 (Nr1)") sceneName = "Level_5 (Nr2)";
+                else if (SceneManager.GetActiveScene().name == "Level_Hub1") sceneName = "Level_4 (Nr1)";
+                else if (SceneManager.GetActiveScene().name == "Level_1_22") sceneName = "Level_Hub1";
+                else if (SceneManager.GetActiveScene().name == "Level_1_23") sceneName = "Level_1_22";
+                else if (SceneManager.GetActiveScene().name == "Level_1_24") sceneName = "Level_1_23";
+                else if (SceneManager.GetActiveScene().name == "Level_1_25") sceneName = "Level_1_24";
+                else if (SceneManager.GetActiveScene().name == "Level_1_21") sceneName = "Level_1_25";
+                else if (SceneManager.GetActiveScene().name == "Level_7 (Boss)") sceneName = "Level_1_21";
+
             }
             LoadScene(sceneName);
     	}
