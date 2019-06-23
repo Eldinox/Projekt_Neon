b8842f04a2435b34906fcab6d78dcb95cd6c8a92
Selim <selim.ariguib@gmail.com>
2019-06-23 17:45:46 +0200

    Background musik

---
+++ Projekt_Neon/Assets/AudioScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("MusicSoucre");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
