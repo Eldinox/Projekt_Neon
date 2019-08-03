49bbc15bf9e0deae272888431886aa1b891c9370
Selim <selim.ariguib@gmail.com>
2019-08-03 19:38:49 +0200

    bosskampf musik, cave music)

--- Projekt_Neon/Assets/AudioScript.cs
+++ Projekt_Neon/Assets/AudioScript.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioScript : MonoBehaviour
 {
     // Start is called before the first frame update
+AudioSource audio1;
+public AudioSource audio3;
+public AudioSource audio2;
+private bool playonce ;
+void Start()
+{
+    audio1 = GetComponent<AudioSource>();
+    playonce = false;
+}
+
 void Awake()
     {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("MusicSoucre");
@@ -14,4 +25,52 @@ void Awake()
         }
         DontDestroyOnLoad(this.gameObject);
     }
+
+private void Update() {
+    if(SceneManager.GetActiveScene().name == "Level_7 (Boss)" )
+    {
+        audio2.Stop();
+        audio1.Stop();
+        transform.position = new Vector3 (132f,4.8f,0f);
+    
+    }
 }
+
+void OnTriggerExit2D(Collider2D col)
+    {
+        if (SceneManager.GetActiveScene().name == "Level_1_22" && col.gameObject.tag == "Player" && audio1.isPlaying)
+        {
+            StartCoroutine(FadeOut(audio1,6f));
+            audio2.Play();
+
+        }
+    }
+void OnTriggerEnter2D(Collider2D col)
+    {
+         if (SceneManager.GetActiveScene().name == "Level_7 (Boss)" && col.gameObject.tag == "Player" && playonce == false )
+        {
+            audio3.Play();
+            playonce = true;
+        }
+    }
+
+ private IEnumerator FadeOut (AudioSource audioSource, float FadeTime) {
+        float startVolume = audioSource.volume;
+ 
+        while (audioSource.volume > 0) {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+ 
+            yield return null;
+        }
+ 
+        audioSource.Stop ();
+        audioSource.volume = startVolume;
+    }
+
+}
+
+
+
+
+    
+
---
+++ Projekt_Neon/Assets/ChangeMusicScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeMusicScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("player Collision");
+        }
+        Debug.Log("player Collision");
+    }
+}
